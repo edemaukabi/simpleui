1ac7aa70c82674cd8a1e5f35e9efe1b19a131243
edemaukabi <edemaukabi@gmail.com>
2023-05-10 09:47:13 +0100

    refactor nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { Logo } from "./Logo";
 
 export const Header = ({}) => {
     const [openSidemenu, setOpenSidemenu] = useState(false)
-    const handleOpenClose = () => {}
+    const [activeClass, setActiveClass] = useState(false)
+    
+    const handleActiveClass = (e) => {
+        e.target.classList.add('active-link');
+    }
+    const handleVerticalActiveClass = (e) => {
+        e.target.classList.add('vertical-active-link');
+    }
+    const handleOpenClose = () => {
+        if (openSidemenu) setOpenSidemenu(false)
+        else setOpenSidemenu(true)
+    }
   return (
     <header>
       <nav className="header-nav">
@@ -15,13 +26,13 @@ export const Header = ({}) => {
         <div>
             <ul class="navbar-menu">
                 <li>
-                    <NavLink>Product</NavLink>
+                    <NavLink  className="nav-link active-link" onClick={handleActiveClass}>Product</NavLink>
                 </li>
                 <li>
-                    <NavLink>About us</NavLink>
+                    <NavLink  className="nav-link" onClick={handleActiveClass}>About us</NavLink>
                 </li>
                 <li>
-                    <NavLink>Contact</NavLink>
+                    <NavLink  className="nav-link" onClick={handleActiveClass}>Contact</NavLink>
                 </li>
             </ul>
         </div>
@@ -29,6 +40,19 @@ export const Header = ({}) => {
             <GiHamburgerMenu size={"1.5rem"} />
         </button>
       </nav>
+      {openSidemenu && <nav class="vertical-nav">
+      <ul class="vertical-navbar-menu">
+                <li>
+                    <NavLink  className="nav-link vertical-active-link" onClick={handleVerticalActiveClass}>Product</NavLink>
+                </li>
+                <li>
+                    <NavLink  className="nav-link" onClick={handleVerticalActiveClass}>About us</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" onClick={handleVerticalActiveClass}>Contact</NavLink>
+                </li>
+            </ul>
+      </nav>}
     </header>
   );
 };
